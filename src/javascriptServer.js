baa2c7f9d3aaf1c60d00490ea275f092e90db142
Dan Voicu <dvoicu@Dans-MacBook-Pro.local>
2021-12-01 22:44:06 -0500

    Still working on server functions

--- src/javascriptServer.js
+++ src/javascriptServer.js
@@ -19,6 +19,12 @@ var playerTurn = 1; //1 or 2.
 //Turn on player two cpu
 var cpuToggle = false;
 
+//Track if the game needs to end.
+var end = false;
+
+//Track who won the game
+var whoWon = 0; // 1 means player 1 won, 2 means player 2 won.
+
 var express = require('express');
 const { send } = require('process');
 var app = express(); //I Think this is for node.js in which the express framwork is used.
@@ -39,8 +45,9 @@ app.post('/post', (req, res) => { //req --> request infromation, res --> server 
                 'action': 'resume',
                 'p1Scr' : p1Score,
                 'p2Scr' : p2Score,
+                'p1Choice': p1Choice,
+                'p2Choice': p2Choice,
                 'pTurn' : playerTurn,
-                'cpu' : cpuToggle,
             });
         }
         else { //Otherwise
@@ -56,14 +63,44 @@ app.post('/post', (req, res) => { //req --> request infromation, res --> server 
         res.send(jsonRes); //send the response back to client
     }
 
-    if(recivedData['action'] == 'store'){
+    //If player one made a move.
+    if(recivedData['action'] == 'p1-move'){
         p1Choice = recivedData[playerChoice]; //store p1's data
-        playerTurn++;
-        var jsonRes = JSON.stringify({
-            'action': 'nextPlay',
-            'cpu': cpuToggle,
-            'pTurn' : playerTurn,
-        });
+        playerTurn++; //change the turn
+
+        if(cpuToggle == true){ //if the cpu is turned on make it's move
+            p2Choice = cpuMove();
+            playerTurn--; //change the turn
+            var Winner = evaluate(p1Choice,p2Choice); //Then evaluate the results
+            Score(Winner);
+            if (end == true){ //Did that move win the game? If so send it to the client.
+                var jsonRes = JSON.stringify({
+                    'action':'finishGame',
+                    'p1Choice': p1Choice,
+                    'p2Choice': p2Choice,
+                    'p1Scr': p1Score,
+                    'p2Scr': p2Score,
+                    'whoWon': whoWon
+                });
+                var gameRunning = false; //Stop game
+            }
+            else { //If not, send request to continue playing
+                var jsonRes = JSON.stringify({
+                    'action':'continue',
+                    'p1Choice': p1Choice,
+                    'p2Choice': p2Choice,
+                    'p1Scr': p1Score,
+                    'p2Scr': p2Score,
+                    'pTurn': pTurn,
+                    'whoWon': whoWon
+                });
+            }
+        }
+        else{ //If the cpu is off, send a request for player two input.
+            var jsonRes = JSON.stringify({'action':'nextTurn'});
+        }
+        
+        res.send(jsonRes);
     }
 
 }).listen(3000);
@@ -77,6 +114,9 @@ function initGame(data){
     gameRunning = true; // set game status as running
     p1Score = 0; //Scores to 0
     p2Score = 0;
+    end = false;
+    playerTurn = 1;
+    whoWon = 0;
     if (data['cpu'] == 'ON'){ //If the cpu was turned on.
         cpuToggle = true; //Store the fact that the cpu is on.
     }
@@ -84,3 +124,70 @@ function initGame(data){
         cpuToggle = false;
     }
 }
+
+//Outputs the result of the two player choices.
+function evaluate(p1, p2){
+    if (p1 == p2){
+        return 0; //tie
+    }
+    else if (p1 == 1){ //rock vs
+        if(p2 == 2){ //paper
+            return 2; //p2 wins
+        }
+        else if(p2 == 3){ //scissors
+            return 1; //p1 wins
+        }
+    }
+    else if (p1 == 2){ //paper vs
+        if(p2 == 1){ //rock
+            return 1; //p1 wins
+        }
+        else if(p2 == 3){ //scissors
+            return 2; //p2 wins
+        }
+    }
+    else if (p1 == 3){ //scissors vs
+        if (p2 == 1) { //rock
+            return 2;
+        }
+        else if(p2 == 2){ //paper
+            return 1; //p1 wins
+        }
+    }
+    else{
+        return -1;
+    }
+}
+
+//Controlls the score of the game
+function Score(win){
+    switch(win){
+        case 0:{
+            break; //No score applied
+        }
+        case 1:{
+            p1Score++;
+            break;
+        }
+        case 2:{
+            p2Score++;
+            break;
+        }
+    }
+
+    //Check if this scoring won the game.
+    if(p1Score == 2){
+        end = true;
+        whoWon = 1;
+    }
+    else if(p2Score == 2){
+        end = true
+        whoWon = 2;
+    }
+}
+
+//Returns a random number from set [1, 2, 3]
+function cpuMove() {
+    return Math.floor(Math.random()*3 + 1);
+}
+
